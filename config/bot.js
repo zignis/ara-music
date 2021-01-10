662a6372f4c1d53e51c1cc203c1ed71215469faa
HexM7 <77036902+HexM7@users.noreply.github.com>
2021-01-10 12:17:01 +0530

    Update bot.js

--- config/bot.js
+++ config/bot.js
@@ -1,13 +1,12 @@
 module.exports = {
     emojis: {
-        off: '<:Cross:795858908081684500>',
-        error: '<:Warn:795862561282064415>',
-        queue: '<:Queue:795900934865682482>',
-        music: ':musical_note:',
-        success: '<:Tick:795858929442619402>',
-        partner: '<:Partner:795860926573183026>',
-        musicIcon: '<a:MusicIcon:795858056264548383>',
-        loading: '<a:Loading:795857964140331078>',
+        off: ':x:',
+        error: ':warn:',
+        queue: ':bamboo:',
+        musicIcon: ':musical_note:',
+        success: ':white_check_mark:',
+        partner: ':handshake:',
+        loading: ':turtle:',
     },
 
     discord: {
