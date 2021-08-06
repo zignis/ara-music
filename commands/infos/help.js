8f3728b89bc146c7a934d9c2132f91aa5f9bb2c6
HexM7 <77036902+HexM7@users.noreply.github.com>
2021-08-06 19:38:46 +0530

    Optimizations

--- commands/infos/help.js
+++ commands/infos/help.js
@@ -1,5 +1,4 @@
-const { Guild } = require("discord.js");
-var Discord = require("discord.js")
+const Discord = require("discord.js")
 
 module.exports = {
     name: 'help',
@@ -15,9 +14,8 @@ module.exports = {
             const musicTotal = message.client.commands.filter(x => x.category == 'Music').size;
             const filtersTotal = client.filters.length;
             const total = message.client.commands.filter(x => x.category == 'Music').size + message.client.commands.filter(x => x.category == 'Utility').size;
-
             let embed = new Discord.MessageEmbed()
-            .setAuthor(`${client.user.username} Help`, message.guild.iconURL())
+            .setAuthor(`${client.user.username} Help`, message.guild.iconURL({dynamic:true}))
             .setDescription(`Ara Help List\nPrefix is \`*\`\nUse \`*help [command-name]\` for details about a specific command.\n\n${client.emotes.partner} **•** [Invite](https://discord.com/oauth2/authorize?client_id=795527817697427476&permissions=8&scope=bot)\n${client.emotes.partner} **•** [Support Server](https://discord.gg/N5GMPZD5sX)`)
             .setFooter(`${client.user.username} • Total Commands - ${total} • Version : Beta`, client.user.avatarURL())
             .setThumbnail(client.user.avatarURL())
@@ -27,14 +25,10 @@ module.exports = {
                 { name: `${client.emotes.musicIcon} **|** Music [${musicTotal}]`, value: music },
                 { name: `${client.emotes.musicIcon} **|** Filters [${filtersTotal}]`, value: client.filters.map((x) => '`' + x + '`').join(', ') }
             )
-            
-
-   message.channel.send(embed)
+            message.channel.send(embed)
         } else {
             const command = message.client.commands.get(args.join(" ").toLowerCase()) || message.client.commands.find(x => x.aliases && x.aliases.includes(args.join(" ").toLowerCase()));
-
-            if (!command) return message.channel.send(`${client.emotes.error} - I did not find this command !`);
-
+            if (!command) return message.channel.send(`${client.emotes.error} - I could not find this command!`);
             message.channel.send({
                 embed: {
                     color: 'E400FF',
@@ -42,7 +36,7 @@ module.exports = {
                     fields: [
                         { name: 'Name', value: "```" + command.name + "```", inline: true },
                         { name: 'Category', value: "```" + command.category + "```" , inline: true },
-                        { name: 'Aliase(s)', value: command.aliases.length < 1 ? '```None```' : "```" + command.aliases.join(', ') + "```", inline: true },
+                        { name: 'Aliases', value: command.aliases.length < 1 ? '```None```' : "```" + command.aliases.join(', ') + "```", inline: true },
                         { name: 'Utilisation', value: "```" + command.utilisation.replace('*', client.config.discord.prefix) +  "```", inline: true },
                     ],
                     timestamp: new Date(),
