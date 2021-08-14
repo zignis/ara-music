dd7b7ffac1ca19ebcac4b20b6b877451efa75f87
HexM7 <77036902+HexM7@users.noreply.github.com>
2021-08-14 18:57:42 +0530

    Update filter.js

--- commands/music/filter.js
+++ commands/music/filter.js
@@ -7,64 +7,42 @@ module.exports = {
     utilisation: '*filter [filter-name]',
 
     execute(client, message, args) {
+        let embed = new Discord.MessageEmbed().setColor("E400FF");
         if (!message.member.voice.channel) {
-            let embed = new Discord.MessageEmbed()
-            .setDescription(`${client.emotes.error} You're not connected to a voice channel!`)
-            .setColor("E400FF")
+            embed.setDescription(`${client.emotes.error} You're not connected to a voice channel!`)
             return message.channel.send(embed);
         }
-
         if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) {
-            let embed = new Discord.MessageEmbed()
-            .setDescription(`${client.emotes.error} You're in a different voice channel!`)
-            .setColor("E400FF")
+            embed.setDescription(`${client.emotes.error} You're in a different voice channel!`)
             return message.channel.send(embed);
         }
-
         if (!client.player.getQueue(message)) {
-            let embed = new Discord.MessageEmbed()
-            .setDescription(`${client.emotes.error} Queue is currently empty!`)
-            .setColor("E400FF")
+            embed.setDescription(`${client.emotes.error} Queue is currently empty!`)
             return message.channel.send(embed);
         }
-
         if (!args[0]) {
-            let embed = new Discord.MessageEmbed()
-            .setDescription(`${client.emotes.error} Invalid filter!`)
-            .setColor("E400FF")
+            embed.setDescription(`${client.emotes.error} Invalid filter!`)
             return message.channel.send(embed);
         }
-
         const filterToUpdate = client.filters.find((x) => x.toLowerCase() === args[0].toLowerCase());
-
         if (!filterToUpdate) {
-            let embed = new Discord.MessageEmbed()
-            .setDescription(`${client.emotes.error} Invalid filter! Use \`*help\` to get a list of available filters`)
-            .setColor("E400FF")
+            embed.setDescription(`${client.emotes.error} Invalid filter! Use \`*help\` to get a list of filters available`);
             return message.channel.send(embed);
         }
-
         const filtersUpdated = {};
         try {
               filtersUpdated[filterToUpdate] = client.player.getQueue(message).filters[filterToUpdate] ? false : true;
-
               client.player.setFilters(message, filtersUpdated);
-
               if (filtersUpdated[filterToUpdate]) {
-                  let embed = new Discord.MessageEmbed()
-                  .setDescription(`${client.emotes.success} Filter applied!`)
-                  .setColor("E400FF")
-                  return message.channel.send(embed);
-                  
-              }
-              else {
-                  let embed = new Discord.MessageEmbed()
-                  .setDescription(`${client.emotes.success} Filter removed!`)
-                  .setColor("E400FF")
+                  embed.setDescription(`${client.emotes.success} Filter applied!`)
+                  return message.channel.send(embed); 
+              } else {
+                  embed.setDescription(`${client.emotes.success} Filter removed!`)
                   return message.channel.send(embed);
               } 
         } catch(e) {
-          return;
+              embed.setDescription(`${client.emotes.error} Caught some errors:\n\`${e}\``);
+              message.channel.send(embed);
         }
     },
 };
