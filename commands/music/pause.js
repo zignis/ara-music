68fa3d5857f7f72f7ae2023d7e98e60e391829f9
HexM7 <77036902+HexM7@users.noreply.github.com>
2021-08-14 19:05:49 +0530

    Update pause.js

--- commands/music/pause.js
+++ commands/music/pause.js
@@ -7,39 +7,25 @@ module.exports = {
     utilisation: '*pause',
 
     execute(client, message) {
+       let embed = new Discord.MessageEmbed().setColor("E400FF");
         if (!message.member.voice.channel) {
-            let embed = new Discord.MessageEmbed()
-            .setDescription(`${client.emotes.error} You're not connected to a voice channel!`)
-            .setColor("E400FF")
+            embed.setDescription(`${client.emotes.error} You're not connected to a voice channel!`);
             return message.channel.send(embed);
         }
-
         if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) {
-            let embed = new Discord.MessageEmbed()
-            .setDescription(`${client.emotes.error} You're in a different voice channel!`)
-            .setColor("E400FF")
+            embed.setDescription(`${client.emotes.error} You're in a different voice channel!`);
             return message.channel.send(embed);
         }
-
         if (!client.player.getQueue(message)) {
-            let embed = new Discord.MessageEmbed()
-            .setDescription(`${client.emotes.error} Queue is currently empty!`)
-            .setColor("E400FF")
+            embed.setDescription(`${client.emotes.error} Queue is currently empty!`);
             return message.channel.send(embed);
         }
-
-
         if (client.player.getQueue(message).paused) {
-            let embed = new Discord.MessageEmbed()
-            .setDescription(`${client.emotes.error} Track is already paused!`)
-            .setColor("E400FF")
+            embed.setDescription(`${client.emotes.error} Track is already paused!`);
             return message.channel.send(embed);
         }
-
         client.player.pause(message);
-        let embed = new Discord.MessageEmbed()
-        .setDescription(`${client.emotes.success} Paused __**[${client.player.getQueue(message).playing.title}](${client.player.getQueue(message).playing.url})**__!`)
-        .setColor("E400FF")
-        return message.channel.send(embed);
+        embed.setDescription(`${client.emotes.success} Paused __**[${client.player.getQueue(message).playing.title}](${client.player.getQueue(message).playing.url})**__!`)
+        message.channel.send(embed);
     },
 };
