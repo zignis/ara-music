551ddf663cec33f0a2989f67d87789c6afca0680
HexM7 <77036902+HexM7@users.noreply.github.com>
2021-08-14 19:02:07 +0530

    Update loop.js

--- commands/music/loop.js
+++ commands/music/loop.js
@@ -6,54 +6,39 @@ module.exports = {
     category: 'Music',
     utilisation: '*loop',
 
-    execute(client, message, args) {
+    execute(client, message, args) { 
+      let embed = new Discord.MessageEmbed().setColor("E400FF");
         if (!message.member.voice.channel) {
-            let embed = new Discord.MessageEmbed()
-            .setDescription(`${client.emotes.error} You're not connected to a voice channel!`)
-            .setColor("E400FF")
+            embed.setDescription(`${client.emotes.error} You're not connected to a voice channel!`);
             return message.channel.send(embed);
         }
-
         if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) {
-            let embed = new Discord.MessageEmbed()
-            .setDescription(`${client.emotes.error} You're in a different voice channel!`)
-            .setColor("E400FF")
+            embed.setDescription(`${client.emotes.error} You're in a different voice channel!`);
             return message.channel.send(embed);
         }
-
-        if (!client.player.getQueue(message)) {
-            let embed = new Discord.MessageEmbed()
-            .setDescription(`${client.emotes.error} Queue is currently empty!`)
-            .setColor("E400FF")
+        if (!client.player.getQueue(message)) {         
+            embed.setDescription(`${client.emotes.error} Queue is currently empty!`);
             return message.channel.send(embed);
         }
         try {
             if (args.join(" ").toLowerCase() === 'queue') {
                 if (client.player.getQueue(message).loopMode) {
                     client.player.setLoopMode(message, false);
-                    let embed = new Discord.MessageEmbed()
-                    .setDescription(`${client.emotes.success} Disabled repeat mode!`)
-                    .setColor("E400FF")
+                    embed.setDescription(`${client.emotes.success} Repeat mode disabled!`);
                     return message.channel.send(embed);
                 } else {
                     client.player.setLoopMode(message, true);
-                    let embed = new Discord.MessageEmbed()
-                    .setDescription(`${client.emotes.success} Enabled repeat mode! [Queue will be repeated]`)
-                    .setColor("E400FF")
+                    embed.setDescription(`${client.emotes.success} Repeat mode enabled! [Queue will be repeated]`);
                     return message.channel.send(embed);
                 };
             } else {
                 if (client.player.getQueue(message).repeatMode) {
                     client.player.setRepeatMode(message, false);
-                    let embed = new Discord.MessageEmbed()
-                    .setDescription(`${client.emotes.success} Disabled repeat mode!`)
-                    .setColor("E400FF")
+                    embed.setDescription(`${client.emotes.success} Repeat mode disabled!`);
                     return message.channel.send(embed);
                 } else {
                     client.player.setRepeatMode(message, true);
-                    let embed = new Discord.MessageEmbed()
-                    .setDescription(`${client.emotes.success} Enabled repeat mode! [Current track will be repeated]`)
-                    .setColor("E400FF")
+                    embed.setDescription(`${client.emotes.success} Repeat mode enabled! [Current track will be repeated]`);
                     return message.channel.send(embed);
                 };
             };
